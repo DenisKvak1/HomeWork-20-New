ffb3d93c84e45501d35eaf3e8bca347acad11a02
DenisKvak1 <denis.kvak457@gmail.com>
2023-11-22 08:06:55 +0200

    Fixed
    Update app.js
    
    Update app.js
    
    3
    
    Update app.js
    
    3
    
    Update app.js
    
    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,15 +6,21 @@ term=document.getElementById('term')
 button=document.getElementById('btn')
 table=document.getElementById('tableBody')
 tableO=document.getElementById('table')
+warn=document.getElementById('warn')
 
 button.addEventListener("click", function() {
-    if(+sum.value>0 && +procent.value>0 && +term.value>0){
+    if(sum.value.includes('.') || sum.value.includes(',') || term.value.includes('.') || term.value.includes(',') || +sum.value>9999999999 ){
+        warn.textContent = "Введите корректные данные :)";
+        tableO.classList.add('d-none');
+    }
+    else if(+sum.value>0 && +procent.value>0 && +term.value>0 && +procent.value<1000 &&  +term.value<300.01){
+        warn.textContent = "";
         t=Math.round(term.value);
         tableO.classList.remove('d-none');
         creditData=[]
         let dolg=+sum.value;
         let telo=+(sum.value/t)
-        let proc=+(dolg*((procent.value/12)/100));
+        let proc=Math.round((dolg*((procent.value/12)/100))*100)/100;
         let plat=+(telo+proc);
         creditData[0]={
             'dolg':Math.round(dolg*100)/100,
@@ -23,11 +29,12 @@ button.addEventListener("click", function() {
             'plat':Math.round(plat*100)/100
         }   
         for(let i=1; i<t; i++){     
-           dolg=dolg-telo;      
-           proc=+(dolg*((procent.value/12)/100));
+           dolg=Math.round((dolg-telo) *100)/100;      
+           proc=Math.round((dolg*((procent.value/12)/100))*100)/100;
            plat=+(telo+proc);
-           if(telo>dolg){
+           if(telo>dolg || i === t - 1){
                 telo=dolg
+                plat=+(telo+proc);
            }
            creditData[i]={
             'dolg':Math.round(dolg*100)/100,
@@ -52,8 +59,8 @@ button.addEventListener("click", function() {
         let html=`
             <tr>
                 <th scope="row">Итого</th>
-                <td></td>
-                <td>${Math.round(creditData.reduce((a, cv) => a + cv['telo'], 0))}</td>
+                <td>0</td>
+                <td>${Math.round(creditData.reduce((a, cv) => a + cv['telo'], 0)*100)/100}</td>
                 <td>Переплата: ${Math.round(creditData.reduce((a, cv) => a + cv['proc'], 0)*100)/100}</td>
                 <td></td>
             </tr>`
